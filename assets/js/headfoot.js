5bee8ea879bf0319e33a6361db416d94c0f7ea52
grav0921 <91196011+grav0921@users.noreply.github.com>
2022-03-21 20:51:25 +0700

    Add files via upload

---
+++ assets/js/headfoot.js
@@ -0,0 +1,7 @@
+// header
+// let url = document.URL;
+document.getElementById("home").innerHTML = "Home";
+document.getElementById("about").innerHTML = "About";
+document.getElementById("industries").innerHTML = "Industries";
+document.getElementById("services").innerHTML = "Services";
+document.getElementById("contact").innerHTML = "Contact";
